8f867963dce6d109ac9e493323cb582688286391
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-10-20 01:55:45 +0000

    fix

--- components/cards/index.js
+++ components/cards/index.js
@@ -1,28 +1,22 @@
 import { styles } from "./style/index.js"
 
 export class Card extends HTMLElement {
-  static observeAttributes = ['isShow']
 
   constructor(){
     super()
   }
   
   connectedCallback() {
-    console.log(this)
-    /*
-    if(this.getAttribute('isShow')){
-      Object.assign(this.style, {display: 'block'})
-    }*/
-    /*
+    
     this.shadow =  this.attachShadow({mode: "open"})
     const slot = document.createElement('slot')
-    
+
     const wrap =  document.createElement('article')
     
     Object.assign(wrap.style, styles.wrap)
     wrap.appendChild(slot)
-    
+
     this.shadow.appendChild(wrap)
-    */
+    
   }
 }
