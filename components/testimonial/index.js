e98cbe3c8c94982292449fbd35f75986aa1b6d68
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-10-20 02:00:41 +0000

    fix

--- components/testimonial/index.js
+++ components/testimonial/index.js
@@ -1,6 +1,7 @@
 import { styles } from "./style/index.js"
 
 export class Testimonial extends HTMLElement {
+  static observedAttributes = ["isShow"]
   constructor(){
     super()
 
@@ -19,42 +20,53 @@ export class Testimonial extends HTMLElement {
 
   }
 
-  build() {
-    const ocupation = document.createElement('p')
-    ocupation.innerText = this.ocupation
-    Object.assign(ocupation.style, styles.profile.description.ocupation)
+  /*
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(
+      `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+    );
+  }
+  */
 
-    const name = document.createElement('h4')
-    name.innerText = this.name
-    Object.assign(name.style, styles.profile.description.name)
+  connectedCallback() {
+    if(this.hasAttribute('isShow')){
 
-    const descriptionWrap = document.createElement('section')
-    descriptionWrap.appendChild(name)
-    descriptionWrap.appendChild(ocupation)
-    Object.assign(descriptionWrap.style, styles.profile.description.wrap)
-
-    const pictureProfile = document.createElement('img')
-    pictureProfile.setAttribute('src', this.profilePicture)
-    pictureProfile.setAttribute('alt', this.pictureAlt)
-
-    const pictureProfileWrap =  document.createElement('figure')
-    Object.assign(pictureProfileWrap.style, styles.profile.picture.wrap)
-    pictureProfileWrap.appendChild(pictureProfile)
-
-    const profileWrap = document.createElement('section')
-    Object.assign( profileWrap.style, styles.profile.wrap)
-    profileWrap.appendChild(pictureProfileWrap)
-    profileWrap.appendChild(descriptionWrap)
-
-    const testimonialwrap = document.createElement('p')
-    testimonialwrap.innerText = this.testimonial
-    Object.assign( testimonialwrap.style, styles.testimonial)
-
-    const wrap = document.createElement('article')
-    wrap.appendChild(profileWrap)
-    wrap.appendChild(testimonialwrap)
-    Object.assign(wrap.style, styles.wrap)
-
-    this.shadow.appendChild(wrap)
+      const ocupation = document.createElement('p')
+      ocupation.innerText = this.ocupation
+      Object.assign(ocupation.style, styles.profile.description.ocupation)
+      
+      const name = document.createElement('h4')
+      name.innerText = this.name
+      Object.assign(name.style, styles.profile.description.name)
+      
+      const descriptionWrap = document.createElement('section')
+      descriptionWrap.appendChild(name)
+      descriptionWrap.appendChild(ocupation)
+      Object.assign(descriptionWrap.style, styles.profile.description.wrap)
+      
+      const pictureProfile = document.createElement('img')
+      pictureProfile.setAttribute('src', this.profilePicture)
+      pictureProfile.setAttribute('alt', this.pictureAlt)
+      
+      const pictureProfileWrap =  document.createElement('figure')
+      Object.assign(pictureProfileWrap.style, styles.profile.picture.wrap)
+      pictureProfileWrap.appendChild(pictureProfile)
+      
+      const profileWrap = document.createElement('section')
+      Object.assign( profileWrap.style, styles.profile.wrap)
+      profileWrap.appendChild(pictureProfileWrap)
+      profileWrap.appendChild(descriptionWrap)
+      
+      const testimonialwrap = document.createElement('p')
+      testimonialwrap.innerText = this.testimonial
+      Object.assign( testimonialwrap.style, styles.testimonial)
+      
+      const wrap = document.createElement('article')
+      wrap.appendChild(profileWrap)
+      wrap.appendChild(testimonialwrap)
+      Object.assign(wrap.style, styles.wrap)
+      
+      this.shadow.appendChild(wrap)
+    }
   }
 }
